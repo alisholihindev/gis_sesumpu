2bd462ec6e52896b4170b569f63aad0bf16e0acd
Ali Sholihin <olinz.07@gmail.com>
2025-05-06 17:42:11 +0800

    fix: switch to adapter-auto for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,13 +9,7 @@ const config = {
 
 	kit: {
 		// default options are shown
-		adapter: adapter({
-			pages: '.svelte-kit/output',
-			assets: '.svelte-kit/output',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
